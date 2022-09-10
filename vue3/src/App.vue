b7a5ae9f79ddac7f73cdaa1ae7e407639f0783f7
ingeniero26 <ingjerson2014gmail.com>
2022-09-10 14:41:56 -0500

    directiva v-for

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
-    <!---convertir a mayusculas-->
-    <h1 v-show="isShowing" v-html="title"></h1>
-    
-  <!-- <h3 v-else-if="isShowing === null">se muestra si no cumple las condiciones</h3>
-    <h2 v-else>Parte dos v-else</h2>--> 
-    <p>Lorem ipsum dolor sit amet.</p>
+    <h1>Tareas</h1>
+    <ul>
+     <li v-for="(hero,index) in dcHeros" v-bind:key="index"> {{index}}-{{hero.name}} </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: "<h1>Titulo grande con etiqueta</h1>",
-      
-      isShowing: true,
-    }
+  export default {
+    data() {
+      return {
+        dcHeros: [
+          {name: "Tareas1" },
+          {name: "Tareas2" },
+          {name: "Tareas3" },
+          {name: "Tareas4" },
+          {name: "Tareas5" },
+        ],
+      };
+    },
   }
-}
 </script>
